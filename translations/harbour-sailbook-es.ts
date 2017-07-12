0a0019b39f38e72f0f47616a2ac2093dfeaddf0f
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-07-11 20:05:23 -0400

    Update harbour-sailbook-es.ts

--- translations/harbour-sailbook-es.ts
+++ translations/harbour-sailbook-es.ts
@@ -5,7 +5,7 @@
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <source>What&apos;s</source>
@@ -13,328 +13,328 @@
     </message>
     <message>
         <source>Sailbook is an unofficial Facebook client for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sailbook es cliente no oficial de facebook para el sistema operativo Sailfish OS.</translation>
     </message>
     <message>
         <source>Privacy &amp; licensing</source>
-        <translation type="unfinished"></translation>
+        <translation>Privacidad &amp; licensia</translation>
     </message>
     <message>
         <source>Developers</source>
-        <translation type="unfinished"></translation>
+        <translation>Desarrolladores</translation>
     </message>
     <message>
         <source>Donate with Paypal</source>
-        <translation type="unfinished"></translation>
+        <translation>Donar con Paypal</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión</translation>
     </message>
     <message>
         <source>Icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Iconos</translation>
     </message>
     <message>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Traductores</translation>
     </message>
     <message>
         <source>Sailbook icons made by Alain Molteni.</source>
-        <translation type="unfinished"></translation>
+        <translation>Iconos de Sailbook hechos por Alain Molteni.</translation>
     </message>
     <message>
         <source>is free software released under the GNU General Public License (GPL), version 3 or later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es software libre publicado bajo la GNU General Public License (GPL), versión 3 o posterior.</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>unread</source>
-        <translation type="unfinished"></translation>
+        <translation>no leído</translation>
     </message>
     <message>
         <source>message(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>mensaje(s)</translation>
     </message>
 </context>
 <context>
     <name>FBWebview</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Regresar</translation>
     </message>
     <message>
         <source>Facebook logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar sesión</translation>
     </message>
 </context>
 <context>
     <name>ImagePage</name>
     <message>
         <source>Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Imagen</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar</translation>
     </message>
 </context>
 <context>
     <name>ImageSelectorPage</name>
     <message>
         <source>Upload image</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargar imagen</translation>
     </message>
 </context>
 <context>
     <name>InputPage</name>
     <message>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre</translation>
     </message>
     <message>
         <source>The right extension is automatically added.</source>
-        <translation type="unfinished"></translation>
+        <translation>La extensión correcta está auto agregada</translation>
     </message>
     <message>
         <source>Saving image OK!</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardando imagen!</translation>
     </message>
     <message>
         <source>Saving image failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Error al guardar imagen!</translation>
     </message>
     <message>
         <source>Download complete!</source>
-        <translation type="unfinished"></translation>
+        <translation>Descarga completa!</translation>
     </message>
     <message>
         <source>Download failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Error de descarga!</translation>
     </message>
     <message>
         <source>File name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre de archivo</translation>
     </message>
     <message>
         <source>Name of the file</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre del archivo</translation>
     </message>
 </context>
 <context>
     <name>LoadscreenWebview</name>
     <message>
         <source>Loading</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargando</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificaciones</translation>
     </message>
     <message>
         <source>Sailbook will send you notifications when you have a new message, a new notification or a friend request.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sailbook notificará sobre un nuevo mensaje, una nueva notificación o una solicitud de amistad.</translation>
     </message>
     <message>
         <source>Opening external link in</source>
-        <translation type="unfinished"></translation>
+        <translation>Abriendo enlace externo en</translation>
     </message>
     <message>
         <source>external browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegador externo</translation>
     </message>
     <message>
         <source>Video quality</source>
-        <translation type="unfinished"></translation>
+        <translation>Calidad de video</translation>
     </message>
     <message>
         <source>360p (SD)</source>
-        <translation type="unfinished"></translation>
+        <translation>360p (SD)</translation>
     </message>
     <message>
         <source>720p (HD)</source>
-        <translation type="unfinished"></translation>
+        <translation>720p (HD)</translation>
     </message>
     <message>
         <source>Enable notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar notificaciones</translation>
     </message>
     <message>
         <source>Notifications interval</source>
-        <translation type="unfinished"></translation>
+        <translation>Intervalo de notificaciones</translation>
     </message>
     <message>
         <source>Quick</source>
-        <translation type="unfinished"></translation>
+        <translation>Rápido</translation>
     </message>
     <message>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal</translation>
     </message>
     <message>
         <source>Long</source>
-        <translation type="unfinished"></translation>
+        <translation>Largo</translation>
     </message>
     <message>
         <source>Increasing the notifications interval may increase your CPU and battery usage but you will receive notifications quicker.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aumentando el intervalo de notificaciones puede aumentar el uso de CPU y batería, pero recibirás notificaciones más rápido.</translation>
     </message>
     <message>
         <source>Very long</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy largo</translation>
     </message>
     <message>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Apariencia</translation>
     </message>
     <message>
         <source>External links</source>
-        <translation type="unfinished"></translation>
+        <translation>Enlaces externos</translation>
     </message>
     <message>
         <source>Video player</source>
-        <translation type="unfinished"></translation>
+        <translation>Reproductor de video</translation>
     </message>
     <message>
         <source>Enable native videoplayer</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar reproducor de video nativo</translation>
     </message>
     <message>
         <source>will open Facebook and Youtube videos in his native videoplayer. If disabled, they will be opened in the external browser.</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrirá los videos de facebook y youtube en el reprodutor de video nativo. Si está deshabilitado, se abrirán en el navegador externo.</translation>
     </message>
     <message>
         <source>Enable nightmode</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar mode de noche</translation>
     </message>
     <message>
         <source>will use dark colors to reduce eye strain while browsing Facebook in the dark.</source>
-        <translation type="unfinished"></translation>
+        <translation>Usará colores oscuros para reducir la fatiga ocular mientras navega en facebook en la oscuridad.</translation>
     </message>
     <message>
         <source>Back button in</source>
-        <translation type="unfinished"></translation>
+        <translation>Botón regresar en</translation>
     </message>
     <message>
         <source>Show Facebook feed button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón de alimentar en facebook</translation>
     </message>
     <message>
         <source>Show Facebook friends button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón amigos en facebook </translation>
     </message>
     <message>
         <source>Show Facebook messages button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón mensajes en facebook </translation>
     </message>
     <message>
         <source>Show Facebook notifications button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón notificaciones en facebook</translation>
     </message>
     <message>
         <source>Show Facebook search button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón buscar en facebook</translation>
     </message>
     <message>
         <source>Show Facebook events button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón eventos en facebook</translation>
     </message>
     <message>
         <source>Show Facebook groups button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón grupos en facebook</translation>
     </message>
     <message>
         <source>Show Facebook settings button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón ajustes en facebook</translation>
     </message>
     <message>
         <source>When disabled, the notifications for friend requests aren&apos;t shown as well.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuando esta deshabilitado, las notificaciones de solicitudes de amigos no se  mostrarán bien.</translation>
     </message>
     <message>
         <source>When disabled, the notifications for new messages aren&apos;t shown as well.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuando esta deshabilitado, las notificaciones de nuevos mensajes no se  mostrarán bien.</translation>
     </message>
     <message>
         <source>When disabled, the notifications for events, ... aren&apos;t shown as well.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuando esta deshabilitado, las notificaciones de eventos no se  mostrarán bien.</translation>
     </message>
     <message>
         <source>Show Facebook liked pages button</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar botón páginas gustadas en facebook</translation>
     </message>
     <message>
         <source>Show Facebook logout pulldown menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar menú deslizable de cierre de sesión de facebook</translation>
     </message>
 </context>
 <context>
     <name>SplashScreen</name>
     <message>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation>Conectando...</translation>
     </message>
 </context>
 <context>
     <name>VideoPage</name>
     <message>
         <source>Videoplayer</source>
-        <translation type="unfinished"></translation>
+        <translation>ReproductorDevideo</translation>
     </message>
     <message>
         <source>Unable to play this video</source>
-        <translation type="unfinished"></translation>
+        <translation>Incapaz de reproducir este video</translation>
     </message>
     <message>
         <source>Use the pulley menu to open it in the browser.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilizar el menú deslizable para abrirlo en el navegador.</translation>
     </message>
 </context>
 <context>
     <name>messages</name>
     <message>
         <source>Opening external link</source>
-        <translation type="unfinished"></translation>
+        <translation>Abriendo enlace externo</translation>
     </message>
 </context>
 <context>
     <name>notify</name>
     <message>
         <source>You have</source>
-        <translation type="unfinished"></translation>
+        <translation>Usted tiene</translation>
     </message>
     <message>
         <source>message(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>mensaje(s)</translation>
     </message>
     <message>
         <source>friend request(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>solicitud de amigo(s)</translation>
     </message>
     <message>
         <source>notification(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>notificación(s)</translation>
     </message>
 </context>
 </TS>
